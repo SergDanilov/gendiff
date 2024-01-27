f8da2a9098359510c0c7fb9d296728c66c5737c6
Serega <danilovserg1985s@gmail.com>
2024-01-27 18:29:22 +0300

    added file to autoload section

---
+++ src/Gendiff.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Hexlet\Code\Gendiff;
+
+public function runDiffer()
+{
+echo "
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version"
+}
