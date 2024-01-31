3ee6bf2dd35f9b3af74d4b2a079e8ba61d468fa4
Serega <danilovserg1985s@gmail.com>
2024-01-31 20:43:43 +0300

    added aditional parameters to function - filepaths

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -2,7 +2,17 @@
 
 namespace Hexlet\Code\Gendiff;
 
-function runDiffer($result)
+function runDiffer($filePath1, $filePath2, $result)
 {   
+    if (file_exists($filePath1)) {
+        require_once $filePath1;
+    } else {
+        echo "Error";
+    }
+    if (file_exists($filePath2)) {
+        require_once $filePath2;
+    } else {
+        echo "Error";
+    }
     print_r($result);
 }
