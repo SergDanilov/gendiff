d54d86bfe5c999e3fce6777ff83a63dccfa6e546
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-02-03 17:00:57 +0000

    created file variables, try to use its

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -3,16 +3,17 @@
 namespace Hexlet\Code\Gendiff;
 
 function runDiffer($filePath1, $filePath2, $result)
-{   
+{
     if (file_exists($filePath1)) {
-        require_once $filePath1;
+        $file1 = require_once $filePath1;
     } else {
         echo "Error";
     }
     if (file_exists($filePath2)) {
-        require_once $filePath2;
+        $file2 = require_once $filePath2;
     } else {
         echo "Error";
     }
+    var_dump($file1);
     print_r($result);
 }
