a1f3718d799637c429f7252b2b4a5089886c68f8
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-02-03 16:26:20 +0000

    ignored vendor folder

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,41 +1,42 @@
-<?php return array(
-    'root' => array(
-        'name' => 'hexlet/code',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => '7f7b7f89b62574a261d5e18505ecfc5b527a2c45',
-        'type' => 'library',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'dev' => true,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        'docopt/docopt' => array(
-            'pretty_version' => '1.0.5',
-            'version' => '1.0.5.0',
-            'reference' => '0e3db660cf2f2eb07a83253790b7d97cdb398826',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../docopt/docopt',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'hexlet/code' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => '7f7b7f89b62574a261d5e18505ecfc5b527a2c45',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'squizlabs/php_codesniffer' => array(
-            'pretty_version' => '3.8.1',
-            'version' => '3.8.1.0',
-            'reference' => '14f5fff1e64118595db5408e946f3a22c75807f7',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
-            'aliases' => array(),
-            'dev_requirement' => true,
-        ),
+    'reference' => '71e884e788c1cc5a2481bb4bd22230194f50833f',
+    'name' => 'hexlet/code',
+  ),
+  'versions' => 
+  array (
+    'docopt/docopt' => 
+    array (
+      'pretty_version' => '1.0.5',
+      'version' => '1.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0e3db660cf2f2eb07a83253790b7d97cdb398826',
     ),
+    'hexlet/code' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '71e884e788c1cc5a2481bb4bd22230194f50833f',
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => '3.8.1',
+      'version' => '3.8.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '14f5fff1e64118595db5408e946f3a22c75807f7',
+    ),
+  ),
 );
