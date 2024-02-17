0f3084d2f5508b883583767db5b9bd2a09f85cb9
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-02-17 14:45:21 +0000

    added true/false string output for arrays

--- src/Differ.php
+++ src/Differ.php
@@ -33,12 +33,22 @@ function genDiff($filePath1, $filePath2)
     if (file_exists($filePath1)) {
         $file1GetContent =  file_get_contents($filePath1);
         $arr1 = json_decode($file1GetContent, true);
+        foreach($arr1 as $key => $value) {
+            if (is_bool($value) === true) {
+                $arr1[$key] = ($value === true) ? 'true' : 'false';
+            }
+        }
     } else {
         throw new \Exception("Unable to open file: '{$filePath1}'!");
     }
     if (file_exists($filePath2)) {
         $file2GetContent =  file_get_contents($filePath2);
         $arr2 = json_decode($file2GetContent, true);
+        foreach($arr2 as $key => $value) {
+            if (is_bool($value) === true) {
+                $arr2[$key] = ($value === true) ? 'true' : 'false';
+            }
+        }
     } else {
         throw new \Exception("Unable to open file: '{$filePath2}'!");
     }
