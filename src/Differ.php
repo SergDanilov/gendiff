d0f98c3fb1cbc0087b8f9e973e0feb8f4464c6ac
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-02-17 13:48:52 +0000

    item to value changed

--- src/Differ.php
+++ src/Differ.php
@@ -2,21 +2,21 @@
 
 namespace Hexlet\Code\Differ;
 
-function comparison (array $fileOne, array $fileTwo)
+function comparison(array $fileOne, array $fileTwo)
 {
     $result = "{\n";
     $resultEquals = array_intersect($fileOne, $fileTwo);
     $arrNew = [];
-    foreach ($resultEquals as $key => $item) {
-        $arrNew["{$key}: {$item}"] = "   ";
+    foreach ($resultEquals as $key => $value) {
+        $arrNew["{$key}: {$value}"] = "   ";
     }
     $result1 = array_diff($fileOne, $fileTwo);
-    foreach ($result1 as $key => $item) {
-        $arrNew["{$key}: {$item}"] = " - ";
+    foreach ($result1 as $key => $value) {
+        $arrNew["{$key}: {$value}"] = " - ";
     }
     $result2 = array_diff($fileTwo, $fileOne);
-    foreach ($result2 as $key => $item) {
-        $arrNew["{$key}: {$item}"] = " + ";
+    foreach ($result2 as $key => $value) {
+        $arrNew["{$key}: {$value}"] = " + ";
     }
     ksort($arrNew);
     foreach ($arrNew as $key => $value) {
@@ -33,16 +33,14 @@ function genDiff($filePath1, $filePath2)
     if (file_exists($filePath1)) {
         $file1GetContent =  file_get_contents($filePath1);
         $arr1 = json_decode($file1GetContent, true);
-        
     } else {
         throw new \Exception("Unable to open file: '{$filePath1}'!");
     }
     if (file_exists($filePath2)) {
         $file2GetContent =  file_get_contents($filePath2);
         $arr2 = json_decode($file2GetContent, true);
-
     } else {
         throw new \Exception("Unable to open file: '{$filePath2}'!");
     }
-    comparison ($arr1, $arr2);
+    comparison($arr1, $arr2);
 }
