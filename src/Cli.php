466a1e34319b0cc2a4ac88c03b10225169b43cc7
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-02-18 13:28:58 +0000

    cli experiment#1

--- src/Cli.php
+++ src/Cli.php
@@ -1,23 +1,14 @@
-#!/usr/bin/env php
-
 <?php 
 
 /*
-Runner to Differ
+Greeting
 */
+
+namespace Hexlet\Code\Cli;
 
 use function Hexlet\Code\Differ\genDiff;
 use function cli\line;
 use function cli\prompt;
-
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
 
 line('Welcome to the "Difference calculator"!');
 line('"Difference calculator" is a program that determines the difference between two data structures.'); 
