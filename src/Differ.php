27b98db9ac5f00bc6a1ed1d9b7e0ee1c4e670dd2
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-02-25 12:08:47 +0000

    united functions

--- src/Differ.php
+++ src/Differ.php
@@ -2,40 +2,14 @@
 
 namespace Hexlet\Code\Differ;
 
-function comparison(array $fileOne, array $fileTwo)
-{
-    $result = "{\n";
-    $resultEquals = array_intersect($fileOne, $fileTwo);
-    $arrNew = [];
-    foreach ($resultEquals as $key => $value) {
-        $arrNew["{$key}: {$value}"] = "   ";
-    }
-    $result1 = array_diff($fileOne, $fileTwo);
-    foreach ($result1 as $key => $value) {
-        $arrNew["{$key}: {$value}"] = " - ";
-    }
-    $result2 = array_diff($fileTwo, $fileOne);
-    foreach ($result2 as $key => $value) {
-        $arrNew["{$key}: {$value}"] = " + ";
-    }
-    ksort($arrNew);
-    foreach ($arrNew as $key => $value) {
-        $result = $result . " {$value} {$key}\n";
-    }
-
-    $result = $result . "}\n";
-    print_r($result);
-    return $result;
-}
-
 function genDiff($filePath1, $filePath2)
 {
     if (file_exists($filePath1)) {
         $file1GetContent =  file_get_contents($filePath1);
-        $arr1 = json_decode($file1GetContent, true);
-        foreach($arr1 as $key => $value) {
+        $fileOne = json_decode($file1GetContent, true);
+        foreach($fileOne as $key => $value) {
             if (is_bool($value) === true) {
-                $arr1[$key] = ($value === true) ? 'true' : 'false';
+                $fileOne[$key] = ($value === true) ? 'true' : 'false';
             }
         }
     } else {
@@ -43,16 +17,37 @@ function genDiff($filePath1, $filePath2)
     }
     if (file_exists($filePath2)) {
         $file2GetContent =  file_get_contents($filePath2);
-        $arr2 = json_decode($file2GetContent, true);
-        foreach($arr2 as $key => $value) {
+        $fileTwo = json_decode($file2GetContent, true);
+        foreach($fileTwo as $key => $value) {
             if (is_bool($value) === true) {
-                $arr2[$key] = ($value === true) ? 'true' : 'false';
+                $fileTwo[$key] = ($value === true) ? 'true' : 'false';
             }
         }
     } else {
         throw new \Exception("Unable to open file: '{$filePath2}'!");
     }
-    print_r($arr1);
-    print_r($arr2);
-    comparison($arr1, $arr2);
+    // print_r($fileOne);
+    // print_r($fileTwo);
+    $result = "{\n";
+    $resultEquals = array_intersect($fileOne, $fileTwo);
+    $difference = [];
+    foreach ($resultEquals as $key => $value) {
+        $difference["{$key}: {$value}"] = "   ";
+    }
+    $result1 = array_diff($fileOne, $fileTwo);
+    foreach ($result1 as $key => $value) {
+        $difference["{$key}: {$value}"] = " - ";
+    }
+    $result2 = array_diff($fileTwo, $fileOne);
+    foreach ($result2 as $key => $value) {
+        $difference["{$key}: {$value}"] = " + ";
+    }
+    ksort($difference);
+    foreach ($difference as $key => $value) {
+        $result = $result . " {$value} {$key}\n";
+    }
+
+    $result = $result . "}\n";
+    print_r($result);
+    return $result;
 }
