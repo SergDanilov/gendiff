4118ca9145d8746e36a4c328ddee9ad253b99b24
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-03-02 13:22:24 +0000

    changed paarameters

--- src/Differ.php
+++ src/Differ.php
@@ -2,7 +2,7 @@
 
 namespace Hexlet\Code\Differ;
 
-function genDiff($filePath1, $filePath2, $format)
+function genDiff($filePath1, $filePath2)
 {
     if (file_exists($filePath1)) {
         $file1GetContent =  file_get_contents($filePath1);
