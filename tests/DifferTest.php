8649149a1e9f952d2971b66ccbfc8ae95c22ca1c
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-03-23 14:11:06 +0000

    experimental data

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -16,25 +16,18 @@ function getFixturePath($path)
 // имя класса совпадает с именем файла
 class DifferTest extends TestCase
 {
-    // Метод, функция определенная внутри класса
-    // Должна начинаться со слова test
-    // public – чтобы PHPUnit мог вызвать этот тест снаружи
     /**
      * @dataProvider provider
      */
     public function testGenDiff($expected, $filePath1, $filePath2): void
     {
-        // Сначала идет ожидаемое значение (expected)
-        // И только потом актуальное (actual)
-        // $path1 = "file1.json";
-        // $path2 = "file2.json";
+
         $this->assertStringEqualsFile($expected, genDiff($filePath1, $filePath2));
     }
-    public function provider() 
+    public function provider()
     {
         return [
             'jsonTojson' => [getFixturePath("sampleString.txt"), "file1.json", "file1.json"]
-            // [$path1 => "file1.yml", $path2 => "file2.yml"],
         ];
     }
 }
