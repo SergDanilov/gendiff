7841d4e88144e30bd025523577cb4275477447c7
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-03-27 14:05:07 +0000

    added new functional for parsing files

--- src/Differ.php
+++ src/Differ.php
@@ -2,39 +2,30 @@
 
 namespace Hexlet\Code\Differ;
 
+use function Hexlet\Code\Parsers\convert;
+
 function getCorrectPath($path)
 {
     $parts = [__DIR__, '../tests/fixtures', $path];
     return realpath(implode('/', $parts));
 }
+
 function genDiff($filePath1, $filePath2)
 {
     $currentArr = getCorrectPath($filePath1);
     $newArr = getCorrectPath($filePath2);
     if (file_exists($currentArr)) {
         $currentArrGetContent =  file_get_contents($currentArr);
-        $currentData = json_decode($currentArrGetContent, true);
-        foreach ($currentData as $key => $value) {
-            if (is_bool($value) === true) {
-                $currentData[$key] = ($value === true) ? 'true' : 'false';
-            }
-        }
+        $currentData = convert($currentArrGetContent);
     } else {
         throw new \Exception("Unable to open file: '{$currentArr}'!");
     }
     if (file_exists($newArr)) {
         $newArrGetContent =  file_get_contents($newArr);
-        $newData = json_decode($newArrGetContent, true);
-        foreach ($newData as $key => $value) {
-            if (is_bool($value) === true) {
-                $newData[$key] = ($value === true) ? 'true' : 'false';
-            }
-        }
+        $newData = convert($newArrGetContent);
     } else {
         throw new \Exception("Unable to open file: '{$newArr}'!");
     }
-    // print_r($currentData);
-    // print_r($newData);
     $oldKey = [];
     foreach ($currentData as $k1 => $v1) {
         if (array_key_exists($k1, $newData)) {
@@ -50,7 +41,6 @@ function genDiff($filePath1, $filePath2)
         }
     }
     sort($oldKey);
-    // print_r($oldKey);
     $newKey = [];
     foreach ($newData as $k2 => $v2) {
         if (!array_key_exists($k2, $currentData)) {
@@ -66,9 +56,7 @@ function genDiff($filePath1, $filePath2)
         }
     }
     sort($newKey);
-    // print_r($newKey);
     $result = array_merge($oldKey, $newKey);
-    // print_r($result);
     $resStr = "{\n";
     foreach ($result as $key => $val) {
         $str = substr($val, 0, -1);
@@ -81,6 +69,5 @@ function genDiff($filePath1, $filePath2)
         }
     }
     $resStr = $resStr . "}\n";
-    // print_r($resStr);
     return $resStr;
 }
