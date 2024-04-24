2236945f14256fed53e267c255994636384d63f1
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-04-24 13:10:11 +0000

    added and fixing some function in formatter

--- src/Formatter.php
+++ src/Formatter.php
@@ -5,17 +5,47 @@ namespace Hexlet\Code\Formatter;
 use function Hexlet\Code\Parsers\convert;
 
 function format($tree) {
-    $resStr = "{\n";
-        foreach ($tree as $key => $val) {
-            $str = substr($val, 0, -1);
-            if ($val[-1] === '-') {
-                $resStr = $resStr . " - {$str}\n";
-            } elseif ($val[-1] === '+') {
-                $resStr = $resStr . " + {$str}\n";
-            } else {
-                $resStr = $resStr . "   {$val}\n";
-            }
+    $resStr = buildStr($tree);
+    return "{\n{$resStr}\n";
+}
+
+function stringify($value) {
+    switch (gettype($value)) {
+        case "boolean":
+            return $value ? 'true' : 'false';
+        case "NULL":
+            return $value = "null";
+        case "integer":
+            return (string) $value;
+        case "string":
+            return $value;
+        default:
+            throw new \Exception("Unknown value type: {$value}");
+    }
+}
+
+function buildStr($diff) {
+    $result = array_map(function ($item) {
+        $key = $item['key'];
+        switch ($item['type']) {
+            case "added":
+                $stringedVal = stringify($item['value']);
+                return " + {$key}: {$stringedVal}";
+            case "deleted":
+                $stringedVal = stringify($item['value']);
+                return " - {$key}: {$stringedVal}";
+            case "nested":
+                $stringedVal = buildStr($item['children']);
+                return "{$key}: {\n{$stringedVal}\n}";
+            case "changed":
+                $stringedOldVal = stringify($item['oldValue']);
+                $stringedVal = stringify($item['value']);
+                return " - {$key}: {$stringedOldVal}\n + {$key}: {$stringedVal}";
+            case "unchanged":
+                return "{$key}: {$item['value']}";
+            default:
+                throw new \Exception("Unknown item type: {$item['type']}");
         }
-        $resStr = $resStr . "}\n";
-        return $resStr;
+    }, $diff);
+    return implode("\n", $result);
 }
