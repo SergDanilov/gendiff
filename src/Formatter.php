5ad50bd9163a5ed73221030f993cc70706a15654
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-04-26 13:33:32 +0000

    added indent and bulder function

--- src/Formatter.php
+++ src/Formatter.php
@@ -6,43 +6,56 @@ use function Hexlet\Code\Parsers\convert;
 
 function format($tree) {
     $resStr = buildStr($tree);
-    return "{\n{$resStr}\n";
+    return "{\n{$resStr}\n}\n";
 }
 
-function stringify($value) {
-    switch (gettype($value)) {
-        case "boolean":
-            return $value ? 'true' : 'false';
-        case "NULL":
-            return $value = "null";
-        case "integer":
-            return (string) $value;
-        case "string":
-            return $value;
-        default:
-            throw new \Exception("Unknown value type: {$value}");
+function stringify($value, $depth) {
+    if (is_bool($value)) {
+        return $value ? 'true' : 'false';
     }
+    if (is_null($value)) {
+        return 'null';
+    }
+
+    if (is_int($value)) {
+        return (string) $value;
+    }
+
+    if (is_string($value)) {
+        return $value;
+    }
+    $closingIndent = str_repeat(" ", $depth * 4);
+    $strings = array_map(function ($key) use ($value, $depth) {
+        $indent = str_repeat(" ", ($depth + 1) * 4);
+        $stringedVal = stringify($value->$key, $depth + 1);
+        return "{$indent}{$key}: {$stringedVal}";
+    }, array_keys(get_object_vars($value)));
+
+    $result = implode("\n", $strings);
+    return "{\n{$result}\n{$closingIndent}}";
 }
 
-function buildStr($diff) {
-    $result = array_map(function ($item) {
+function buildStr($diff, $depth = 1) {
+    $indent = str_repeat(" ", $depth * 4);
+    $indentInner = str_repeat(" ", $depth * 4 - 2);
+    $result = array_map(function ($item) use ($indent, $indentInner, $depth) {
         $key = $item['key'];
         switch ($item['type']) {
             case "added":
-                $stringedVal = stringify($item['value']);
-                return " + {$key}: {$stringedVal}";
+                $stringedVal = stringify($item['value'], $depth);
+                return "{$indentInner}+ {$key}: {$stringedVal}";
             case "deleted":
-                $stringedVal = stringify($item['value']);
-                return " - {$key}: {$stringedVal}";
+                $stringedVal = stringify($item['value'], $depth);
+                return "{$indentInner}- {$key}: {$stringedVal}";
             case "nested":
-                $stringedVal = buildStr($item['children']);
-                return "{$key}: {\n{$stringedVal}\n}";
+                $stringedVal = buildStr($item['children'], $depth + 1);
+                return "{$indent}{$key}: {\n{$stringedVal}\n{$indent}}";
             case "changed":
-                $stringedOldVal = stringify($item['oldValue']);
-                $stringedVal = stringify($item['value']);
-                return " - {$key}: {$stringedOldVal}\n + {$key}: {$stringedVal}";
+                $stringedOldVal = stringify($item['oldValue'], $depth);
+                $stringedVal = stringify($item['value'], $depth);
+                return "{$indentInner}- {$key}: {$stringedOldVal}\n{$indentInner}+ {$key}: {$stringedVal}";
             case "unchanged":
-                return "{$key}: {$item['value']}";
+                return "{$indent}{$key}: {$item['value']}";
             default:
                 throw new \Exception("Unknown item type: {$item['type']}");
         }
