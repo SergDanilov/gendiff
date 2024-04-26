eb2d0a76721d4820aeb92d1ea72545572c3068d6
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-04-26 15:55:24 +0000

    added format for tests

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -19,15 +19,16 @@ class DifferTest extends TestCase
     /**
      * @dataProvider provider
      */
-    public function testGenDiff($expected, $filePath1, $filePath2): void
+    public function testGenDiff($expected, $filePath1, $filePath2, $format): void
     {
-        $this->assertStringEqualsFile($expected, genDiff($filePath1, $filePath2));
+        $this->assertStringEqualsFile($expected, genDiff($filePath1, $filePath2, $format));
     }
     public function provider()
     {
         return [
-            'nestedJsonTojson' => [getFixturePath("nestedString.txt"), "nested1.json", "nested2.json"],
-            'nestedymlToyml' => [getFixturePath("nestedString.txt"), "nested1.yml", "nested2.yml"]
+            'nestedJsonTojson' => [getFixturePath("nestedString.txt"), "nested1.json", "nested2.json", "stylish"],
+            'nestedymlToyaml' => [getFixturePath("nestedString.txt"), "nested1.yaml", "nested2.yaml", "stylish"],
+            'nestedymlToyml' => [getFixturePath("nestedString.txt"), "nested1.yml", "nested2.yml", "stylish"]
         ];
     }
 }
