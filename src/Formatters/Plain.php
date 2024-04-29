5363e6baa2977d2a95b4f21fe3ea079a5d80f4a2
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-04-29 12:37:30 +0000

    maked some logic to Plain formatter

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -2,6 +2,8 @@
 
 namespace Hexlet\Code\Formatters\Plain;
 
+use function Docopt\array_filter;
+
 function stringify($value)
 {
     if (is_bool($value)) {
@@ -16,36 +18,52 @@ function stringify($value)
     }
 
     if (is_string($value)) {
-        return $value;
+        return "'{$value}'";
     }
     $result = "[complex value]";
     return $result;
 }
 
-function buildPlainText($diff)
+function buildPlainText($diff, $ancestor = "")
 {
-        $result = array_map(function ($item) {
-            $key = $item['key'];
-            switch ($item['type']) {
-                case "added":
-                    $stringedVal = stringify($item['value']);
-                    return "Property '{$key}' was added with value: '{$stringedVal}'";
-                case "deleted":
-                    $stringedVal = stringify($item['value']);
-                    return "Property '{$key}' was removed";
-                case "nested":
-                    $stringedVal = buildPlainText($item['children']);
+    $filteredChildren = \array_filter($diff, fn($child) => is_array($child));
+    $result = array_map(function ($item) use ($ancestor) {
+        $key = $item['key'];
+        switch ($item['type']) {
+            case "added":
+                $stringedVal = stringify($item['value']);
+                if ($ancestor !== "") {
+                    return "Property '{$ancestor}.{$key}' was added with value: {$stringedVal}";
+                } else {
                     return "Property '{$key}' was added with value: {$stringedVal}";
-                case "changed":
-                    $stringedOldVal = stringify($item['oldValue']);
-                    $stringedVal = stringify($item['value']);
-                    return "Property '{$key}' was updated. From '{$stringedOldVal}' to '{$stringedVal}'";
-                case "unchanged":
-                    break;
-                default:
-                    throw new \Exception("Unknown item type: {$item['type']}");
-            }
-        }, $diff);
-        $result = array_filter($result, fn($str) => !is_null($str));
-        return implode("\n", $result);
+                }
+            case "deleted":
+                if ($ancestor !== "") {
+                    return "Property '{$ancestor}.{$key}' was removed";
+                } else {
+                    return "Property '{$key}' was removed";
+                }
+            case "nested":
+                if ($ancestor != "") {
+                    $ancestor = "{$ancestor}.{$key}";
+                } else {
+                    $ancestor = "{$key}";
+                }
+                return buildPlainText($item['children'], $ancestor);
+            case "changed":
+                $stringedOldVal = stringify($item['oldValue']);
+                $stringedVal = stringify($item['value']);
+                if (isset($ancestor)) {
+                    return "Property '{$ancestor}.{$key}' was updated. From {$stringedOldVal} to {$stringedVal}";
+                } else {
+                    return "Property '{$key}' was updated. From {$stringedOldVal} to {$stringedVal}";
+                }
+            case "unchanged":
+                break;
+            default:
+                throw new \Exception("Unknown item type: {$item['type']}");
+        }
+    }, $filteredChildren);
+    $result = \array_filter($result, fn($str) => !is_null($str));
+    return implode("\n", $result);
 }
