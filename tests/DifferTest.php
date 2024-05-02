e432ceaf24286334d920ae48dbc6a516c26bf8f1
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2024-05-02 12:30:17 +0000

    removed tests for yaml

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -29,20 +29,14 @@ class DifferTest extends TestCase
             'simpleJsonToJson' => [getFixturePath("sampleString.txt"), "file1.json", "file2.json"],
             'defaultJsonToJson' => [getFixturePath("stylish.txt"), "nested1.json", "nested2.json"],
             'stylishJsonToJson' => [getFixturePath("stylish.txt"), "nested1.json", "nested2.json", "stylish"],
-            'stylishYamlToYaml' => [getFixturePath("stylish.txt"), "nested1.yaml", "nested2.yaml", "stylish"],
+            // 'stylishYamlToYaml' => [getFixturePath("stylish.txt"), "nested1.yaml", "nested2.yaml", "stylish"],
             'stylishYmlToYml' => [getFixturePath("stylish.txt"), "nested1.yml", "nested2.yml", "stylish"],
             'plainJsonToJson' => [getFixturePath("plain.txt"), "nested1.json", "nested2.json", "plain"],
-            'plainYamlToYaml' => [getFixturePath("plain.txt"), "nested1.yaml", "nested2.yaml", "plain"],
+            // 'plainYamlToYaml' => [getFixturePath("plain.txt"), "nested1.yaml", "nested2.yaml", "plain"],
             'plainYmlToYml' => [getFixturePath("plain.txt"), "nested1.yml", "nested2.yml", "plain"],
             'jsonJsonToJson' => [getFixturePath("json.txt"), "nested1.json", "nested2.json", "json"],
-            'jsonYamlToYaml' => [getFixturePath("json.txt"), "nested1.yaml", "nested2.yaml", "json"],
+            // 'jsonYamlToYaml' => [getFixturePath("json.txt"), "nested1.yaml", "nested2.yaml", "json"],
             'jsonYmlToYml' => [getFixturePath("json.txt"), "nested1.yml", "nested2.yml", "json"],
         ];
     }
-
-    // public function testGenDiffWithoutFormat()
-    // {
-    //     $expected = getFixturePath("stylish.txt");
-    //     $this->assertStringEqualsFile($expected, genDiff("nested1.json", "nested2.json"));
-    // }
 }
